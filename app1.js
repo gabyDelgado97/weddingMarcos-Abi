df8ace0684bcfdd87be4d5ae456fca699a0ed9d8
gabyDelgado97 <g-abby091@outlook.com>
2021-02-22 20:59:38 -0500

    mi primer commit

---
+++ app1.js
@@ -0,0 +1,8 @@
+function mostrar() {
+  var img=document.getElementById("img")
+  var btnW=document.getElementById("btnW")
+  btnW.style.display="block";
+  img.src='img/boda.gif';
+  img.height = 490;
+  img.style.marginTop='0px';
+}
