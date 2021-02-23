6bff499d1423eb12f9005086f91818f68cee9a61
zephyr-art <79492082+zephyr-art@users.noreply.github.com>
2021-02-23 10:02:14 -0500

    tercer commit

--- app1.js
+++
@@ -1,8 +0,0 @@
-function mostrar() {
-  var img=document.getElementById("img")
-  var btnW=document.getElementById("btnW")
-  btnW.style.display="block";
-  img.src='img/boda.gif';
-  img.height = 490;
-  img.style.marginTop='0px';
-}
